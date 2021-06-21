24e1fe2a53d819a59570d7ee7f0a305526db9c01
shanshuirenjia <978987373@qq.com>
2021-06-21 10:42:27 +0800

    update readme file

---
+++ src/setting.local.dist.js
@@ -0,0 +1,23 @@
+/**
+ * local development settings
+ */
+export default {
+  isDevelopment: true,
+  username: 'e9f75af40ab9466e8d4bf1022e3dadba@auth.local',
+  password: '*******',
+  lang: "zh-cn",
+  siteRoot: '/',
+  mediaUrl: 'http://127.0.0.1:80/media/',
+  server: "http://192.168.0.44:80",
+  dtableServer: "http://127.0.0.1:5000",
+  dtableSocket: "http://127.0.0.1:5000",
+  dtableUuid: "84c80017-510d-4afe-afe4-6918d2dd3d9e",
+  accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MjQwMDM4MDYsImR0YWJsZV91dWlkIjoiODRjODAwMTctNTEwZC00YWZlLWFmZTQtNjkxOGQyZGQzZDllIiwidXNlcm5hbWUiOiJlOWY3NWFmNDBhYjk0NjZlOGQ0YmYxMDIyZTNkYWRiYUBhdXRoLmxvY2FsIiwicGVybWlzc2lvbiI6InJ3In0.dhGvs_5hI1Sgvajg7bnA9_BlUj8JzL-n1LgkZxK-BBU",
+  workspaceID: "1",
+  dtableName: "test-1",
+  appId: '12',
+  appType: 'gallery',
+  appToken: '5f50a894-bf16-432d-82b0-284a7f437093',
+  appConfig: "{\"app_name\":\"abc\",\"app_type\":\"gallery\",\"settings\":{\"table_name\":\"Table1\",\"view_name\":\"默认视图\",\"shown_image_name\":\"\",\"shown_title_name\":\"\",\"shown_column_names\":[]}}",
+  isEditAppPage: true,
+};
